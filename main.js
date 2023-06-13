4ef06db9377be677d6b19f5951137c9bc5d5091b
Paula Chaves <pchaves@colegiodelainmaculada.edu.ar>
2023-06-12 22:51:17 -0300

    Se agrega el proyecto

---
+++ main.js
@@ -0,0 +1,22 @@
+// Variables
+var nombre = "Juan";
+var edad = 25;
+
+// Condicionales
+if (edad >= 18) {
+  console.log(nombre + " es mayor de edad.");
+} else {
+  console.log(nombre + " es menor de edad.");
+}
+
+// Ciclos
+for (var i = 0; i < 5; i++) {
+  console.log("El valor de i es: " + i);
+}
+
+// Funciones
+function saludar(nombre) {
+  console.log("Hola, " + nombre + "!");
+}
+
+saludar("María");

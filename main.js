0bc477167e37e7cfb25be79839ee94ea1c9ee319
Dario <pchaves@gmail.edu.ar>
2023-07-03 21:26:13 -0300

    cambios en el index

--- main.js
+++ main.js
@@ -1,22 +1,39 @@
-// Variables
-var nombre = "Juan";
-var edad = 25;
+console.log("Expulsado");
+let numero1 = prompt("100");
+let numero2 = prompt("50");
 
-// Condicionales
-if (edad >= 18) {
-  console.log(nombre + " es mayor de edad.");
-} else {
-  console.log(nombre + " es menor de edad.");
+numero1 = parseFloat(numero1);
+numero2 = parseFloat(numero2);
+let resultado = numero1 + numero2;
+alert("150 " + resultado);
+
+class jugadores {
+  constructor(nombre, apellido) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.club = equipoactual;
+  }
+  mostrar() {
+    console.log(`Nombre: ${this.nombre} ${this.apellido}
+      ${this.club}`);
+  }
 }
 
-// Ciclos
-for (var i = 0; i < 5; i++) {
-  console.log("El valor de i es: " + i);
+class Clase {
+  constructor(jugador, tecnico, club) {
+    this.jugador = jugador;
+    this.tecnico = tecnico;
+    this.club = club;
+  }
+  mostrarClase() {
+    console.log(`club: ${this.club}`);
+    console.log(`${this.jugador.nombre} ${this.jugador.apellido}`);
+    console.log(`${this.tecnico.nombre} ${this.tecnico.apellido}`);
+  }
 }
 
-// Funciones
-function saludar(nombre) {
-  console.log("Hola, " + nombre + "!");
-}
+const jugador1 = new jugador("martin", "palermo");
+const tecnico1 = new tecnico("caruso", "lombardi");
 
-saludar("María");
+const clase1 = new Clase(jugador, tecnico, "JavaScript");
+clase1.mostrarClase();
